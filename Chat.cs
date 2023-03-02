ebb76aa227a69ca47dc4f2e028abed7ea4760542
Graeson Bergen <graesonbergen@gmail.com>
2023-03-01 19:49:45 -0700

    Added Chat class for new API

---
+++ Chat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatGPTWrapper {
+    // Due to OpenAI's new chat completions api, this replaces the old "Prompt" class, but the prompt class is still used for the older models.
+    public class Chat
+    {
+        private string _initialPrompt;
+        private  List<Message> _currentChat = new List<Message>();
+
+        public Chat(string initialPrompt) {
+            _initialPrompt = initialPrompt;
+            Message systemMessage = new Message("system", initialPrompt);
+            _currentChat.Add(systemMessage);
+        }
+        public List<Message> CurrentChat { get { return _currentChat; } }
+
+        public enum Speaker {
+            User,
+            ChatGPT
+        }
+
+        public void AppendMessage(Speaker speaker, string text)
+        {
+        
+            switch (speaker)
+            {
+                case Speaker.User:
+                    Message userMessage = new Message("user", text);
+                    _currentChat.Add(userMessage);
+                    break;
+                case Speaker.ChatGPT:
+                    Message chatGPTMessage = new Message("assistant", text);
+                    _currentChat.Add(chatGPTMessage);
+                    break;
+            }
+        }
+    }
+}
