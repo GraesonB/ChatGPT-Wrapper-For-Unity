1fb86922b515e6409c58b88bf0dbc651f6f07452
Graeson Bergen <graesonbergen@gmail.com>
2023-03-01 20:00:04 -0700

    Added ChatGPT classes for reqs to new API

---
+++ WebRequestClasses/ChatGPTRes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatGPTWrapper {
+    [Serializable]
+    public class ChatGPTRes
+    {
+        public List<ChatGPTChoices> choices;
+    }
+}
